c0ef0c828af3e994c196ec8d5d09f4b68997aea0
MohamedAhamed2100 <107947222+MohamedAhamed2100@users.noreply.github.com>
2024-08-07 23:19:09 +0300

    Update Register_File.v

--- Register_File.v
+++ Register_File.v
@@ -19,19 +19,19 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module Register_File(
-     input  CLK , Reset , WE , // WE : write enable
+     input  clk , rst , WE3 , // WE : write enable
 	  input  [ 4  : 0 ] A1  , A2  , A3 ,  //  A1 : source register 1 , A2 : source register 2 , A3 : direction register 
-	  input  [ 31 : 0 ] WD  ,  
+	 input  [ 31 : 0 ] WD3  ,  
 	  output [ 31 : 0 ] RD1 , RD2
     );
    
 	 reg [ 31 : 0 ] registers [ 31 : 0 ] ; 
 	 integer K ;
 	 
-	 always @( posedge CLK )
+	always @( posedge clk )
 	 begin 
 	 
-	      if ( Reset == 1'b1 )
+		 if ( rst == 1'b1 )
 			begin
 			   for ( K = 0 ; K < 32 ; K = K + 1  )
 				begin
@@ -39,7 +39,7 @@ module Register_File(
 			   end
 			end
 			
-			else if ( WE == 1'b1 )  
+		 else if ( WE3 == 1'b1 )  
 			       registers[A3] <= WD ;
 				
 	 end
